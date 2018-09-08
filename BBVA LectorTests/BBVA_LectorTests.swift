091e6ac162221bba707cad46735798cd7a0232b1
SamArtGS <samuelarturo@icloud.com>
2018-09-07 21:52:26 -0500

    Initial Commit

---
+++ BBVA LectorTests/BBVA_LectorTests.swift
@@ -0,0 +1,36 @@
+//
+//  BBVA_LectorTests.swift
+//  BBVA LectorTests
+//
+//  Created by Samuel Arturo Garrido Sánchez on 07/09/18.
+//  Copyright © 2018 Master Scores. All rights reserved.
+//
+
+import XCTest
+@testable import BBVA_Lector
+
+class BBVA_LectorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
